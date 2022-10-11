9fd666a81ccb953904e868b1fb70ab3cf81ba69a
kefrankefran7609 <48444561+kefrankefran7609@users.noreply.github.com>
2022-10-11 06:04:29 -0600

    Add files via upload

---
+++ startup.js
@@ -0,0 +1,81 @@
+/* Startup Calculator */
+
+/* Quering all inputs */
+const inputs = document.querySelectorAll('.input');
+  
+inputs.forEach((input) => {
+
+	/* Setting the default value of all inputs to 0 */
+  input.value = "0"
+  
+  /* Reading the value of an input when the value changes */
+  input.addEventListener('change', (e) => {
+  
+  /* Reseeting the value of a input to 0 if left empty */
+    if(e.path[0].value === ""){
+    input.value = "0"
+    }    
+  });
+})
+
+/* Firing the calculator when submitting */
+document.querySelector('#calculate').addEventListener('click', (e) => {
+
+	/* Preventing the default behavior from the form */
+ e.preventDefault()
+ 
+ /* Calculating all the values */
+ const total = 
+ +document.querySelector('#rental').value +
+ +document.querySelector('#firstRent').value +
+ +document.querySelector('#utility').value +
+ +document.querySelector('#other1').value +
+ +document.querySelector('#other2').value +
+ +document.querySelector('#other3').value +
+ +document.querySelector('#other4').value +
+ +document.querySelector('#other5').value +
+ +document.querySelector('#car').value +
+ +document.querySelector('#equipment').value +
+ +document.querySelector('#office').value +
+ +document.querySelector('#initial').value +
+ +document.querySelector('#itEquipment').value +
+  +document.querySelector('#itSystem').value +
+ +document.querySelector('#itWebsite').value +
+ +document.querySelector('#professional').value +
+ +document.querySelector('#licence').value +
+ +document.querySelector('#inicialPromotion').value
+ /* Making the total display as the result */
+ document.querySelector('#total').textContent = total.toFixed(2) 
+})
+
+
+document.querySelector('#calculateMonthly').addEventListener('click', (e) => {
+
+	/* Preventing the default behavior from the form */
+ e.preventDefault()
+
+ const total =
++document.querySelector('#other5').value +
++document.querySelector('#other6').value +
++document.querySelector('#other7').value +
++document.querySelector('#other8').value +
++document.querySelector('#other9').value +
++document.querySelector('#other10').value +
++document.querySelector('#salary').value +
++document.querySelector('#payroll').value +
++document.querySelector('#recruitment').value +
++document.querySelector('#rent').value +
++document.querySelector('#bills').value +
++document.querySelector('#insurance').value +
++document.querySelector('#transport').value +
++document.querySelector('#maintenance').value +
++document.querySelector('#software').value +
++document.querySelector('#website').value +
++document.querySelector('#accounting').value +
++document.querySelector('#legal').value +
++document.querySelector('#promotion').value 
+  
+  document.querySelector('#monthly').textContent = total.toFixed(2)
+	
+ 
+})
