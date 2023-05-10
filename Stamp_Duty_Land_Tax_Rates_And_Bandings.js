7b902acab8e427cb223fdfc368ec48c2324275e0
kefrankefran7609 <48444561+kefrankefran7609@users.noreply.github.com>
2023-05-10 11:41:10 -0600

    Update Stamp_Duty_Land_Tax_Rates_And_Bandings.js

--- Stamp_Duty_Land_Tax_Rates_And_Bandings.js
+++ Stamp_Duty_Land_Tax_Rates_And_Bandings.js
@@ -17,13 +17,17 @@ fetch("https://api.airtable.com/v0/apphKFqKdClqcgF26/tblIt28vfTJiKpCe1", request
   .then(response => response.text())
   .then(data =>  { 
   const parsed = JSON.parse(data)
+  console.log(parsed)
   /* Give the values to the elements on the page from our APi call */
+   document.querySelector('[title]').textContent = parsed.records[4].fields["Purchase consideration"]
+   document.querySelector('[title1]').textContent = parsed.records[4].fields["Rate – on portion above threshold"]
+   document.querySelector('[title2]').textContent = parsed.records[4].fields["Rate if second or subsequent property"]   
    document.querySelector('[first]').textContent = parsed.records[2].fields["Purchase consideration"]
    document.querySelector('[firstrate1]').textContent = parsed.records[2].fields["Rate – on portion above threshold"]
    document.querySelector('[firstrate2]').textContent = parsed.records[2].fields["Rate if second or subsequent property"]
-   document.querySelector('[second]').textContent = parsed.records[4].fields["Purchase consideration"]
-   document.querySelector('[secondrate1]').textContent = parsed.records[4].fields["Rate – on portion above threshold"]
-   document.querySelector('[secondrate2]').textContent = parsed.records[4].fields["Rate if second or subsequent property"]
+   document.querySelector('[second]').textContent = parsed.records[5].fields["Purchase consideration"]
+   document.querySelector('[secondrate1]').textContent = parsed.records[5].fields["Rate – on portion above threshold"]
+   document.querySelector('[secondrate2]').textContent = parsed.records[5].fields["Rate if second or subsequent property"]
    document.querySelector('[third]').textContent = parsed.records[1].fields["Purchase consideration"]
    document.querySelector('[third1]').textContent = parsed.records[1].fields["Rate – on portion above threshold"]
    document.querySelector('[third2]').textContent = parsed.records[1].fields["Rate if second or subsequent property"]
@@ -33,7 +37,7 @@ fetch("https://api.airtable.com/v0/apphKFqKdClqcgF26/tblIt28vfTJiKpCe1", request
    document.querySelector('[fifth]').textContent = parsed.records[3].fields["Purchase consideration"]
    document.querySelector('[fifth1]').textContent = parsed.records[3].fields["Rate – on portion above threshold"]
    document.querySelector('[fifth2]').textContent = parsed.records[3].fields["Rate if second or subsequent property"]   
-   document.querySelector('[note]').innerHTML = parsed.records[5].fields.Notes  
+   document.querySelector('[note]').innerHTML = parsed.records[6].fields.Notes  
   })
   .catch(error => console.log('error', error))  
 /*****  End of Webflow push function *****/
