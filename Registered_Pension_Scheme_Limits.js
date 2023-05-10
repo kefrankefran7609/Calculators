0c489f560b79f293d17039727f87e700f8be8240
kefrankefran7609 <48444561+kefrankefran7609@users.noreply.github.com>
2023-05-10 09:19:10 -0600

    Update Registered_Pension_Scheme_Limits.js

--- Registered_Pension_Scheme_Limits.js
+++ Registered_Pension_Scheme_Limits.js
@@ -19,16 +19,21 @@ fetch("https://api.airtable.com/v0/apphKFqKdClqcgF26/tble7rzATUidKph2b", request
   .then(data =>  { 
   const parsed = JSON.parse(data)
   /* Give the values to the elements on the page from our APi call */
+  document.querySelector('[yearfrom]').textContent = parsed.records[1].fields["Year from"]
+   document.querySelector('[yearto]').textContent = parsed.records[1].fields["Year to"]
    document.querySelector('[la]').textContent = parsed.records[2].fields.Name
-   document.querySelector('[la21-22]').textContent = parsed.records[2].fields["2021/22"]
-   document.querySelector('[la22-23]').textContent = parsed.records[2].fields["2022/23"]
+   document.querySelector('[la21-22]').textContent = parsed.records[2].fields["Year from"]
+   document.querySelector('[la22-23]').textContent = parsed.records[2].fields["Year to"]
    document.querySelector('[aa]').textContent = parsed.records[0].fields.Name
-   document.querySelector('[aa21-22]').textContent = parsed.records[0].fields["2021/22"]
-   document.querySelector('[aa22-23]').textContent = parsed.records[0].fields["2022/23"]
+   document.querySelector('[aa21-22]').textContent = parsed.records[0].fields["Year from"]
+   document.querySelector('[aa22-23]').textContent = parsed.records[0].fields["Year to"]
+   /*
 	 document.querySelector('[excessdrawn]').textContent = parsed.records[1].fields.Name
-   document.querySelector('[excessdrawn21-23]').textContent = parsed.records[1].fields["2021/22"]
+   document.querySelector('[excessdrawn21-23]').textContent = parsed.records[1].fields["Year from"]
+   */
    document.querySelector('[excessinputs]').textContent = parsed.records[4].fields.Name
-   document.querySelector('[excessinputs21-23]').textContent = parsed.records[4].fields["2021/22"]
+   document.querySelector('[excessinputs21-23]').textContent = parsed.records[4].fields["Year from"]
+   
    document.querySelector('[bottomnote]').innerHTML = parsed.records[3].fields.Notes
  })
   
