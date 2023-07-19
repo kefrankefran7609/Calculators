b85c60b85e4d37c32d8fa178052bde0df7734487
kefrankefran7609 <48444561+kefrankefran7609@users.noreply.github.com>
2023-07-19 14:46:30 -0600

    Update Stamp_Duty_Land_Tax_Rates_And_Bandings.js

--- Stamp_Duty_Land_Tax_Rates_And_Bandings.js
+++ Stamp_Duty_Land_Tax_Rates_And_Bandings.js
@@ -18,26 +18,51 @@ fetch("https://api.airtable.com/v0/apphKFqKdClqcgF26/tblIt28vfTJiKpCe1", request
   .then(data =>  { 
   const parsed = JSON.parse(data)
   console.log(parsed)
-  /* Give the values to the elements on the page from our APi call */
-   document.querySelector('[title]').textContent = parsed.records[4].fields["Purchase consideration"]
-   document.querySelector('[title1]').textContent = parsed.records[4].fields["Rate – on portion above threshold"]
-   document.querySelector('[title2]').textContent = parsed.records[4].fields["Rate if second or subsequent property"]   
-   document.querySelector('[first]').textContent = parsed.records[2].fields["Purchase consideration"]
-   document.querySelector('[firstrate1]').textContent = parsed.records[2].fields["Rate – on portion above threshold"]
-   document.querySelector('[firstrate2]').textContent = parsed.records[2].fields["Rate if second or subsequent property"]
-   document.querySelector('[second]').textContent = parsed.records[5].fields["Purchase consideration"]
-   document.querySelector('[secondrate1]').textContent = parsed.records[5].fields["Rate – on portion above threshold"]
-   document.querySelector('[secondrate2]').textContent = parsed.records[5].fields["Rate if second or subsequent property"]
-   document.querySelector('[third]').textContent = parsed.records[1].fields["Purchase consideration"]
-   document.querySelector('[third1]').textContent = parsed.records[1].fields["Rate – on portion above threshold"]
-   document.querySelector('[third2]').textContent = parsed.records[1].fields["Rate if second or subsequent property"]
-   document.querySelector('[fourth]').textContent = parsed.records[0].fields["Purchase consideration"]
-   document.querySelector('[fourth1]').textContent = parsed.records[0].fields["Rate – on portion above threshold"]
-   document.querySelector('[fourth2]').textContent = parsed.records[0].fields["Rate if second or subsequent property"]
-   document.querySelector('[fifth]').textContent = parsed.records[3].fields["Purchase consideration"]
-   document.querySelector('[fifth1]').textContent = parsed.records[3].fields["Rate – on portion above threshold"]
-   document.querySelector('[fifth2]').textContent = parsed.records[3].fields["Rate if second or subsequent property"]   
-   document.querySelector('[note]').innerHTML = parsed.records[6].fields.Notes  
+  /* Give the values to the elements on the page from our API call */
+   document.querySelector('[pageheading]').textContent = parsed.records[24].fields.Name 
+   document.querySelector('[location1]').textContent = parsed.records[35].fields.Name  
+   document.querySelector('[topnote]').innerHTML = parsed.records[20].fields.Notes 
+   document.querySelectorAll('[title]')forEach(el => el.textContent = parsed.records[23].fields.Name)
+   document.querySelectorAll('[title1]')forEach(el => el.textContent = parsed.records[23].fields["1st row"])
+   document.querySelector('[title2]').textContent = parsed.records[23].fields["2nd row"]   
+   document.querySelector('[first]').textContent = parsed.records[9].fields.Name
+   document.querySelector('[firstrate1]').textContent = parsed.records[9].fields["1st row"]
+   document.querySelector('[firstrate2]').textContent = parsed.records[9].fields["2nd row"] 
+   document.querySelector('[second]').textContent = parsed.records[8].fields.Name
+   document.querySelector('[secondrate1]').textContent = parsed.records[8].fields["1st row"]
+   document.querySelector('[secondrate2]').textContent = parsed.records[8].fields["2nd row"]
+   document.querySelector('[third]').textContent = parsed.records[2].fields.Name
+   document.querySelector('[third1]').textContent = parsed.records[2].fields["1st row"]
+   document.querySelector('[third2]').textContent = parsed.records[2].fields["2nd row"]
+   document.querySelector('[fifth]').textContent = parsed.records[18].fields.Name
+   document.querySelector('[fifth1]').textContent = parsed.records[18].fields["1st row"]
+   document.querySelector('[fifth2]').textContent = parsed.records[18].fields["2nd row"]  
+   document.querySelector('[englandbottomnote]').innerHTML = parsed.records[31].fields.Notes 
+   document.querySelector('[location2]').textContent = parsed.records[28].fields.Name  
+   document.querySelector('[walestopnote]').innerHTML = parsed.records[17].fields.Notes
+   document.querySelector('[walesfirst]').textContent = parsed.records[11].fields.Name
+   document.querySelector('[walesfirstrate1]').textContent = parsed.records[11].fields["1st row"]
+   document.querySelector('[walessecond]').textContent = parsed.records[4].fields.Name
+   document.querySelector('[walessecondrate1]').textContent = parsed.records[4].fields["1st row"]
+   document.querySelector('[walesthird]').textContent = parsed.records[32].fields.Name
+   document.querySelector('[walesthird1]').textContent = parsed.records[32].fields["1st row"]
+   document.querySelector('[walesfourth]').textContent = parsed.records[30].fields.Name
+   document.querySelector('[walesfourth1]').textContent = parsed.records[30].fields["1st row"]
+   document.querySelector('[walesfifth]').textContent = parsed.records[16].fields.Name
+   document.querySelector('[walesfifth1]').textContent = parsed.records[16].fields["1st row"] 
+   document.querySelector('[walesaddtopnote]').innerHTML = parsed.records[7].fields.Notes
+   document.querySelector('[walesaddfirst]').textContent = parsed.records[34].fields.Name
+   document.querySelector('[walesaddfirstrate1]').textContent = parsed.records[34].fields["1st row"]
+   document.querySelector('[walesaddsecond]').textContent = parsed.records[25].fields.Name
+   document.querySelector('[walesaddsecondrate1]').textContent = parsed.records[25].fields["1st row"]
+   document.querySelector('[walesaddthird]').textContent = parsed.records[32].fields.Name
+   document.querySelector('[walesaddthird1]').textContent = parsed.records[32].fields["1st row"]
+   document.querySelector('[walesaddfourth]').textContent = parsed.records[30].fields.Name
+   document.querySelector('[walesaddfourth1]').textContent = parsed.records[30].fields["1st row"]
+   document.querySelector('[walesaddfifth]').textContent = parsed.records[16].fields.Name
+   document.querySelector('[walesaddfifth1]').textContent = parsed.records[16].fields["1st row"]
+   document.querySelector('[walesaddsixth]').textContent = parsed.records[16].fields.Name
+   document.querySelector('[walesaddsixth1]').textContent = parsed.records[16].fields["1st row"]
   })
   .catch(error => console.log('error', error))  
 /*****  End of Webflow push function *****/
