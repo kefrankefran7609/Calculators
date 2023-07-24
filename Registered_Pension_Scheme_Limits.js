9b68e1dcccaa155792bdc00eb5eed44134adca75
kefrankefran7609 <48444561+kefrankefran7609@users.noreply.github.com>
2023-07-24 08:49:37 -0600

    Update Registered_Pension_Scheme_Limits.js

--- Registered_Pension_Scheme_Limits.js
+++ Registered_Pension_Scheme_Limits.js
@@ -17,6 +17,7 @@ var requestOptions = {
 fetch("https://api.airtable.com/v0/apphKFqKdClqcgF26/tble7rzATUidKph2b", requestOptions)
   .then(response => response.text())
   .then(data =>  { 
+   const parsed = JSON.parse(data)
   /* Give the values to the elements on the page from our APi call */
    document.querySelectorAll('[pageheading]').forEach(el => el.textContent = parsed.records[1].fields.Name
    document.querySelector('[yearfrom]').textContent = parsed.records[1].fields["Year from"]
