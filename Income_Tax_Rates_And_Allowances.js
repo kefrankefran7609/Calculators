9bf8226e175d7206e6ad5cc20a6ce5e0325a6ea0
kefrankefran7609 <48444561+kefrankefran7609@users.noreply.github.com>
2023-07-26 05:09:53 -0600

    Update Income_Tax_Rates_And_Allowances.js

--- Income_Tax_Rates_And_Allowances.js
+++ Income_Tax_Rates_And_Allowances.js
@@ -81,6 +81,31 @@ fetch("https://api.airtable.com/v0/apphKFqKdClqcgF26/tbl859m8WhwIV59tT", request
   })
   .catch(error => console.log('error', error))  
 
+  fetch("https://api.airtable.com/v0/apphKFqKdClqcgF26/tblf4WESF6Q7u0fZp", requestOptions)
+  .then(response => response.text())
+  .then(data =>  { 
+  const parsed = JSON.parse(data)
+  /* Give the values to the elements on the page from our APi call */
+   document.querySelector('[rbc]').textContent = parsed.records[3].fields["Remittance basis charge"]
+   document.querySelector('[rbcto]').textContent = parsed.records[3].fields["Year from"]
+   document.querySelector('[rbcfrom]').textContent = parsed.records[3].fields["Year to"]
+   document.querySelector('[rate]').textContent = parsed.records[3].fields["Rate bands"]
+   document.querySelector('[rateto]').textContent = parsed.records[3].fields["Year from"]
+   document.querySelector('[ratefrom]').textContent = parsed.records[3].fields["Year to"]
+   document.querySelector('[remittance1]').textContent = parsed.records[7].fields["Remittance basis charge"]
+   document.querySelector('[remittance121-22]').textContent = parsed.records[7].fields["Year from"]
+   document.querySelector('[remittance122-23]').textContent = parsed.records[7].fields["Year to"]
+   document.querySelector('[remittance2]').textContent = parsed.records[8].fields["Remittance basis charge"]
+   document.querySelector('[remittance221-22]').textContent = parsed.records[8].fields["Year from"]
+   document.querySelector('[remittance222-23]').textContent = parsed.records[8].fields["Year to"]
+   document.querySelector('[remittance3]').textContent = parsed.records[6].fields["Remittance basis charge"]
+   document.querySelector('[remittance321-22]').textContent = parsed.records[6].fields["Year from"]
+   document.querySelector('[remittance322-23]').textContent = parsed.records[6].fields["Year to"]
+   document.querySelector('[topnote]').innerHTML = parsed.records[4].fields.Notes
+ })
+  
+  .catch(error => console.log('error', error))
+
   /***** Hiding rows that don't receive content from Airtbale *****/
 setTimeout(() => {
 document.querySelectorAll('.tax-data_table-row').forEach((el) => {
