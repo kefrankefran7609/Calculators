aafd2a3f8ea26879bd83f297a937a2500d8aeb34
kefrankefran7609 <48444561+kefrankefran7609@users.noreply.github.com>
2023-12-06 07:45:28 -0600

    Update investor.js

--- investor.js
+++ investor.js
@@ -1,69 +1,56 @@
 document.querySelector('input[type="radio"][value="uk"]').checked = true
 
+//Open nav main dropdown
 document.querySelector('[openmaindropdown]').addEventListener('click', () => {
   document.querySelector('[maindropdown]').classList.toggle('show')
   document.querySelector('[maindropdownarrow]').classList.toggle('show')
-  document.querySelector('[countrydropdown]').classList.remove('show')
-  document.querySelector('[seconddropdownarrow]').classList.remove('show')
-  document.querySelector('.nav_top-second-dropdown-menu').classList.remove('show')
-  document.querySelector('[change]').textContent = "Change"
 })
 
+//Close nav main dropdown
 document.querySelector('[closedropdown]').addEventListener('click', () => {
   document.querySelector('[maindropdown]').classList.remove('show')
   document.querySelector('[maindropdownarrow]').classList.remove('show')
-  document.querySelector('[countrydropdown]').classList.remove('show')
-  document.querySelector('[seconddropdownarrow]').classList.remove('show')
-  document.querySelector('.nav_top-second-dropdown-menu').classList.remove('show')
-  document.querySelector('[change]').textContent = "Change"
+  document.querySelector('[locationdropdowncontent]').classList.remove('show') 
+  document.querySelector('[usertypedropdowncontent]').classList.remove('show') 
 })
 
-var click = 0
-document.querySelector('[opencountrydropdown]').addEventListener('click', () => {
-  click++ 
-  document.querySelector('[countrydropdown]').classList.toggle('show')
-  document.querySelector('[seconddropdownarrow]').classList.toggle('show')
-  document.querySelector('.nav_top-second-dropdown-menu').classList.toggle('show')
-  if(click == 1) document.querySelector('[change]').textContent = "Hide"
-  if(click == 2) {
-    document.querySelector('[change]').textContent = "Change"
-    click = 0
-  }
+document.querySelector('[locationtoggle]').addEventListener('click', () => {
+  document.querySelector('[locationdropdowncontent]').classList.toggle('show') 
 })
+
+document.querySelector('[usertypetoggle]').addEventListener('click', () => {
+  document.querySelector('[usertypedropdowncontent]').classList.toggle('show') 
+})
+
+
 
 // Changing country
- document.querySelectorAll('[radioflag]').forEach(el => {
+document.querySelectorAll('[radioflag]').forEach(el => {
   el.addEventListener('change', (e) => {
-  const parent = document.querySelector('input[radioflag]:checked').closest('.radio-button')
-  document.querySelector('[country]').textContent = parent.querySelector('.radio-label').textContent
-  document.querySelector('[flag]').src = parent.querySelector('.nav_top-flag').src
-  document.querySelector('[flag]').srcset = parent.querySelector('.nav_top-flag').srcset
-  /*
-  document.querySelectorAll('.nav_top-dropdown-type').forEach(els => {
-    els.style.display = "none"
+  if(!e.target.classList.contains('is-active')){
+    document.querySelectorAll('.radio-button').forEach(els => {
+    els.classList.remove('is-active')
+    })  
+  e.target.parentElement.classList.add('is-active')
+  document.querySelectorAll('[flag]').forEach(el => {
+  el.src = e.target.nextElementSibling.src
+})
+document.querySelectorAll('[flag]').forEach(el => {
+  el.srcset = e.target.nextElementSibling.srcset
+})
+  }
   })
-  document.querySelectorAll(`[${e.target.value}]`).forEach(els => {
-    els.style.display = "flex"
-  })*/
-  })
-}) 
+})
 
 // Selecting a user type
 document.querySelectorAll('.nav_top-dropdown-type').forEach(el => {
   el.addEventListener('click', (e) => {
-    console.log(e.target)
   if(!e.target.classList.contains('is-active')){  
   document.querySelectorAll('.nav_top-dropdown-type').forEach(els => {
     els.classList.remove('is-active')
   })
   e.target.classList.add('is-active')
-  document.querySelector('[investortype]').textContent = e.target.querySelector('.text-weight-semibold').textContent
-  document.querySelector('[maindropdown]').classList.remove('show')
-  document.querySelector('[maindropdownarrow]').classList.remove('show')
-  document.querySelector('[countrydropdown]').classList.remove('show')
-  document.querySelector('[seconddropdownarrow]').classList.remove('show')
-  document.querySelector('.nav_top-second-dropdown-menu').classList.remove('show')
-  document.querySelector('[change]').textContent = "Change"
+  document.querySelector('[investortype]').textContent = e.target.querySelector('[usertype]').textContent
   }
   })
 })
